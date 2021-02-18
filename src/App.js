c05cab7dc9076d0bf0df4674466fb2ee1292fa1d
Melvin Alfaro <melvinalfaro@iMac-de-Melvin.local>
2021-02-18 17:51:46 -0600

    updated menu item to navigate withRoute HOC

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch> 
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+
+// Route tiene 3 propiedades: exact - path - component. path y component son muy obvias, 
+// exact puede ser true o false (true si se pone solo exact), en dado caso, React renderiza cualquier ruta que haga
+// match con el path.
+// Ejm: en el caso anterior, si exact es false, dado que los path son "/" y "/hats", se cumple las condiciones en el URL
+// para que react haga render de todo, porque el url localhost:3000/hats, incluye / e incluye /hats
+
+
+// Switch se encarga de que se haga render en cambio, del primer match de URL que encuentre, si encuentra que path es /abc (y no hay un routing creado para /abc)
+// entonces nada mas (si está de primero en el código) se va a renderizar sin importar que se ponga delante en el URL, la primera coincidencia en este caso localhost:3000/
+// esto es útil si no se quiere accidentalmete renderizar multiples componentes, o si el usuario ingresa propiedades que no existen a la URL
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div 
     className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
         <div className= 'background-image'
         style={{
@@ -18,7 +20,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
 
 // backgroundImage: react tiene la propiedad style, que nos permite al igual que en los elementos HTML crear estilos
 // para cada elemento, uno de ellos es backgroundImaage, que no solo permite dar una imagen de background para un 
@@ -26,3 +28,6 @@ export default MenuItem;
 // cada componente uno por uno y darle su backgound.
 // backgoundImage se utiliza con JS templates (``) y llamando una url en este caso. 
 // (no se puede solo poner el prop como backgroundImage : imageUrl)
+
+// withRouter es un High Order Component (HOC), lo cual es básicamente una función que toma un componente como argumento
+// y retorna un componente modificado.
