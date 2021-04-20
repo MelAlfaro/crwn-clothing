eeed21b31f6f4749ed43a9fd0527364691d9ab89
Melvin Alfaro <melvinalfaro@iMac-de-Melvin.local>
2021-04-19 22:11:27 -0600

    updated shop data with new data, updated cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,18 @@
+// utility functions: permiten mantener nuestros archivos limpios y organizar funciones que se podrían 
+// necesitar en múltiples files en un location
+
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(
+      cartItem => cartItem.id === cartItemToAdd.id
+    );
+  
+    if (existingCartItem) {
+      return cartItems.map(cartItem =>
+        cartItem.id === cartItemToAdd.id
+          ? { ...cartItem, quantity: cartItem.quantity + 1 }
+          : cartItem
+      );
+    }
+  
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  };
