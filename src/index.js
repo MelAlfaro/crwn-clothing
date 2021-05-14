043996f053fa05f4160c7d6687122ba78f32776c
Melvin Alfaro <melvinalfaro@iMac-de-Melvin.local>
2021-05-14 14:56:05 -0600

    moved directory and shop data into its own reducer, created respective selectors and updated corresponding components with redux flow for directory/shop

--- src/index.js
+++ src/index.js
@@ -2,23 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'; // este componente va a hacer wrap sobre todo el app, ya que se busca que todo tenga acceso
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 import App from './App';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 
 ReactDOM.render(
   // el Provider, que se obtiene de react redux, es un componente que una vez pasado al objeto store, 
   //será capaz de darle al store, contexto al resto de la app para despachar acciones al store, o jalar
   // valores del store y hacia nuestros componentes.
+  // Es importante tambien hacer wrap de toda la app con el persistor, de forma que tenga acceso al persistence flow como tal.
   <Provider store={store}>
       <BrowserRouter>
-        <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist'; // redux persist para almancenar datos de sesión o de forma local: https://www.npmjs.com/package/redux-persist
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store); // crea una version persistente del store previamente creado
+
+export default { store, persistor };
